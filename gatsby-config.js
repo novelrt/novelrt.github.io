f0a1a3cb732256ceab6c5f4ca7856f448ab784cd
Ken Johnson <k.rjohnson94@outlook.com>
2021-01-14 13:32:49 -0500

    Pushing template - GatsbyJS + TailwindCSS
    Also added Novel-Chan and Title font

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "NovelRT",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
