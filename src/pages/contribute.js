26edb21d9bbc92687b597cbc9760e1a1e6b057a6
Trace <lukaszdabrowski881@gmail.com>
2021-10-09 09:22:25 +0100

    fix missing text-center on h1 on mobile

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -7,7 +7,7 @@ const ContributePage = () => {
             <title>Contribute | NovelRT</title>
             <Layout>
                 <div id="main-branding" className="flex flex-col items-center py-12 md:px-12 lg:px-36 xl:px-46 2xl:px-64 bg-gradient-to-b from-green-400 via-blue-500 to-indigo-600">
-                    <h1 className="my-4 text-5xl tracking-wide">
+                    <h1 className="my-4 text-5xl tracking-wide text-center">
                         How to contribute.
                     </h1>
                     <h2 className="text-lg mx-16 xl:w-1/2 text-center">
