e7276f7f32a2f74e96da97ad3868ffe70f0d945c
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-15 16:45:57 +0100

    set up prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: "es5",
+    singleQuote: false,
+    printWidth: 80,
+    tabWidth: 4,
+    quoteProps: "consistent",
+    endOfLine: "lf"
+};
