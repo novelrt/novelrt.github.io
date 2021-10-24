85076f87512f646881b089e82880efb7949a772f
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-24 05:54:12 +0100

    refactor NavbarElement to be a functional style component

--- src/components/navbar/NavbarElement.jsx
+++ src/components/navbar/NavbarElement.jsx
@@ -1,22 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 
-class NavbarElement extends Component {
-    render() {
-        return (
-            <li className="block mt-4 md:inline-block md:mt-0 mr-4">
-                <AniLink
-                    swipe
-                    direction="left"
-                    id={this.props.name}
-                    to={this.props.linksTo}
-                    className="text-lg outline-none md:text-2xl hover:text-gray-400 duration-300"
-                >
-                    {this.props.name}
-                </AniLink>
-            </li>
-        );
-    }
-}
+const NavbarElement = ({ name, linksTo }) => (
+    <li className="block mt-4 md:inline-block md:mt-0 mr-4">
+        <AniLink
+            swipe
+            direction="left"
+            id={name}
+            to={linksTo}
+            className="text-lg outline-none md:text-2xl hover:text-gray-400 duration-300"
+        >
+            {name}
+        </AniLink>
+    </li>
+);
 
 export default NavbarElement;
