a29a696b2c8a0aabe6cb47cada69106bca4b6c1e
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-24 05:56:02 +0100

    make sure Layout function is consistent with the other functions

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Footer from "./Footer";
 import Navbar from "./navbar/Navbar";
 
-export default function Layout({ children }) {
-    return (
-        <div className="text-gray-100">
-            <Navbar />
-            {children}
-            <Footer />
-        </div>
-    );
-}
+const Layout = ({ children }) => (
+    <div className="text-gray-100">
+        <Navbar />
+        {children}
+        <Footer />
+    </div>
+);
+
+export default Layout;
