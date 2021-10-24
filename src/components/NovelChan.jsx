2b6dfa59cb0d05c739c6e698091bdd226f9393f2
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-24 05:46:54 +0100

    refactor NovelChan component so that the function style is consistent

--- src/components/NovelChan.jsx
+++ src/components/NovelChan.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
-function Logo() {
-    return (
-        <StaticImage
-            src="../images/novel-chan.png"
-            className="w-64"
-            loading="eager"
-            placeholder="none"
-            fadeIn={false}
-            durationFadeIn={0}
-            alt="Novel-Chan: NovelRT's mascot!"
-        />
-    );
-}
+const Logo = () => (
+    <StaticImage
+        src="../images/novel-chan.png"
+        className="w-64"
+        loading="eager"
+        placeholder="none"
+        fadeIn={false}
+        durationFadeIn={0}
+        alt="Novel-Chan: NovelRT's mascot!"
+    />
+);
 
 export default Logo;
