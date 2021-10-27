ef5044f03bdc597bbc7eca9c57abf69edd1ff929
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-27 02:21:26 +0100

    typo fix

--- src/components/icons/HeroiconsThinOutlineIcons.jsx
+++ src/components/icons/HeroiconsThinOutlineIcons.jsx
@@ -44,7 +44,7 @@ export const ExclamationCircleIconThin = () => (
         <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={1.25}
+            strokeWidth="1.25"
             d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
     </svg>
@@ -60,7 +60,7 @@ export const ChatIconThin = () => (
         <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={1.25}
+            strokeWidth="1.25"
             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
         />
     </svg>
