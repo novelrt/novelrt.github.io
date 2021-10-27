690d62f3f6230cb8deb2adac61bc06197362b25a
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-27 02:12:35 +0100

    add a thin variant of the PR icon

--- src/components/icons/PullRequestIcon.jsx
+++ src/components/icons/PullRequestIcon.jsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 
 const PrIcon = () => (
-    <svg
-        className="w-10 h-9"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
         <title>Git Pull Request</title>
         <circle
             cx="128"
@@ -52,6 +48,62 @@ const PrIcon = () => (
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="42"
+        />
+    </svg>
+);
+
+export const PrIconThin = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="ionicon"
+        viewBox="0 0 512 512"
+    >
+        <title>Git Pull Request</title>
+        <circle
+            cx="128"
+            cy="416"
+            r="48"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="32"
+        />
+        <path
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="32"
+            d="M128 144v224M288 160l-64-64 64-64"
+        />
+        <circle
+            cx="128"
+            cy="96"
+            r="48"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="32"
+        />
+        <circle
+            cx="384"
+            cy="416"
+            r="48"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="32"
+        />
+        <path
+            d="M240 96h84a60 60 0 0160 60v212"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="32"
         />
     </svg>
 );
