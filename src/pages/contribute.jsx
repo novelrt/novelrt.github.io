cef3526a04083a51199ec9cd7f76667ac9d459b6
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-27 02:13:11 +0100

    extract detailed features into a reusable component

--- src/pages/contribute.jsx
+++ src/pages/contribute.jsx
@@ -1,205 +1,112 @@
 import * as React from "react";
 import { ExclamationCircleIcon, ChatIcon } from "@heroicons/react/outline";
-import PullRequestIcon from "../components/icons/PullRequestIcon";
+import PullRequestIcon, {
+    PrIconThin,
+} from "../components/icons/PullRequestIcon";
 import Layout from "../components/Layout";
 import Feature from "../components/Feature";
 import FeaturesRow from "../components/FeaturesRow";
 import Hero from "../components/Hero";
+import {
+    ChatIconThin,
+    ExclamationCircleIconThin,
+} from "../components/icons/HeroiconsThinOutlineIcons";
+import DetailedFeatures from "../components/DetailedFeatures";
 
-const ContributePage = () => (
-    <main className="bg-gray-100 min-h-screen">
-        <title>Contribute | NovelRT</title>
-        <Layout>
-            <Hero
-                title="How to contribute."
-                description="NovelRT is under active development as we continue to enhance the 2D/Visual Novel development experience. With that said, here are a few ways you can contribute:"
-            >
-                <FeaturesRow>
-                    <Feature featureName="Report an issue" linksTo="#issue">
-                        <ExclamationCircleIcon />
-                    </Feature>
-                    <Feature featureName="Create a Pull Request" linksTo="#pr">
-                        <PullRequestIcon />
-                    </Feature>
-                    <Feature featureName="Reach out to us" linksTo="#reach-out">
-                        <ChatIcon />
-                    </Feature>
-                </FeaturesRow>
-            </Hero>
+const ContributePage = () => {
+    const detailedFeatures = [
+        {
+            id: "issue",
+            icon: <ExclamationCircleIconThin />,
+            title: "Report an issue.",
+            content: (
+                <>
+                    Running into a reproducible bug? Please feel free to&nbsp;
+                    <a
+                        className="underline hover:text-gray-500 duration-300"
+                        href="https://github.com/NovelRT/NovelRT/issues"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        open an issue on our GitHub repository
+                    </a>
+                    ! We&apos;d love to be able to resolve your problem.
+                </>
+            ),
+        },
+        {
+            id: "pr",
+            icon: <PrIconThin />,
+            title: "Create a Pull Request.",
+            content: (
+                <>
+                    Want to help out the NovelRT project? Thanks! We appreciate
+                    all the help we can get. To get started,&nbsp;
+                    <a
+                        className="underline hover:text-gray-500 duration-300"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/novelrt/NovelRT/labels/good%20first%20issue"
+                    >
+                        simply have a look at our good first issues
+                    </a>
+                    &nbsp;and get involved!
+                </>
+            ),
+        },
+        {
+            id: "reach-out",
+            icon: <ChatIconThin />,
+            title: "Reach out to us.",
+            content: (
+                <>
+                    Feeling stuck? Don&apos;t know where to start? Have some
+                    questions?&nbsp;
+                    <a
+                        className="underline hover:text-gray-500 duration-300"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://discord.novelrt.dev/"
+                    >
+                        Join our Discord server
+                    </a>
+                    &nbsp;where our contributors will happily help you.
+                </>
+            ),
+        },
+    ];
 
-            <div
-                id="secondary-branding"
-                className="flex flex-col gap-16 xl:gap-20 items-center py-28 md:px-12 lg:px-36 xl:px-46 2xl:px-64 mx-24 md:mx-36 xl:mx-64 text-gray-800"
-            >
-                <div
-                    id="issue"
-                    className="flex flex-col xl:flex-row items-center xl:flex xl:items-center"
+    return (
+        <main className="bg-gray-100 min-h-screen">
+            <title>Contribute | NovelRT</title>
+            <Layout>
+                <Hero
+                    title="How to contribute."
+                    description="NovelRT is under active development as we continue to enhance the 2D/Visual Novel development experience. With that said, here are a few ways you can contribute:"
                 >
-                    <div className="xl:mx-12">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 xl:py-6 xl:px-6 w-16 h-16 xl:w-36 xl:h-36 shadow">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={1.25}
-                                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                />
-                            </svg>
-                        </div>
-                    </div>
-                    <div>
-                        <h3 className="mt-2 xl:mt-0 font-semibold xl:font-normal text-center xl:text-left text-3xl xl:text-4xl">
-                            Report an issue
-                        </h3>
-                        <p className="mt-2 text-center xl:text-left">
-                            Running into a reproducible bug? Please feel free to
-                            <a
-                                className="underline hover:text-gray-500 duration-300"
-                                href="https://github.com/NovelRT/NovelRT/issues"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                open an issue on our GitHub repository
-                            </a>
-                            ! We&apos;d love to be able to resolve your problem.
-                        </p>
-                    </div>
-                </div>
-                <div
-                    id="pr"
-                    className="flex flex-col xl:flex-row items-center xl:flex xl:items-center"
-                >
-                    <div className="xl:hidden">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 w-16 h-16 shadow">
+                    <FeaturesRow>
+                        <Feature featureName="Report an issue" linksTo="#issue">
+                            <ExclamationCircleIcon />
+                        </Feature>
+                        <Feature
+                            featureName="Create a Pull Request"
+                            linksTo="#pr"
+                        >
                             <PullRequestIcon />
-                        </div>
-                    </div>
-                    <div>
-                        <h3 className="mt-2 xl:mt-0 font-semibold xl:font-normal text-center xl:text-left text-3xl xl:text-4xl">
-                            Create a Pull Request
-                        </h3>
-                        <p className="mt-2 text-center xl:text-left">
-                            Want to help out the NovelRT project? Thanks! We
-                            appreciate all the help we can get. To get started,
-                            <a
-                                className="underline hover:text-gray-500 duration-300"
-                                target="_blank"
-                                rel="noreferrer"
-                                href="https://github.com/novelrt/NovelRT/labels/good%20first%20issue"
-                            >
-                                simply have a look at our good first issues
-                            </a>
-                            and get involved!
-                        </p>
-                    </div>
-                    <div className="hidden xl:block xl:mx-12">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 xl:py-6 xl:px-6 w-16 h-16 xl:w-36 xl:h-36 shadow">
-                            <div className="flex items-center justify-center">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="ionicon"
-                                    viewBox="0 0 512 512"
-                                >
-                                    <title>Git Pull Request</title>
-                                    <circle
-                                        cx="128"
-                                        cy="416"
-                                        r="48"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="32"
-                                    />
-                                    <path
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="32"
-                                        d="M128 144v224M288 160l-64-64 64-64"
-                                    />
-                                    <circle
-                                        cx="128"
-                                        cy="96"
-                                        r="48"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="32"
-                                    />
-                                    <circle
-                                        cx="384"
-                                        cy="416"
-                                        r="48"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="32"
-                                    />
-                                    <path
-                                        d="M240 96h84a60 60 0 0160 60v212"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="32"
-                                    />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    id="reach-out"
-                    className="flex flex-col xl:flex-row items-center xl:flex xl:items-center"
-                >
-                    <div className="xl:mx-12">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 xl:py-6 xl:px-6 w-16 h-16 xl:w-36 xl:h-36 shadow">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={1.25}
-                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                                />
-                            </svg>
-                        </div>
-                    </div>
-                    <div>
-                        <h3 className="mt-2 xl:mt-0 font-semibold xl:font-normal text-center xl:text-left text-3xl xl:text-4xl">
-                            Reach out to us
-                        </h3>
-                        <p className="mt-2 text-center xl:text-left">
-                            Feeling stuck? Don&apos;t know where to start? Have
-                            some questions?
-                            <a
-                                className="underline hover:text-gray-500 duration-300"
-                                target="_blank"
-                                rel="noreferrer"
-                                href="https://discord.novelrt.dev/"
-                            >
-                                Join our Discord server
-                            </a>
-                            where our contributors will happily help you.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    </main>
-);
+                        </Feature>
+                        <Feature
+                            featureName="Reach out to us"
+                            linksTo="#reach-out"
+                        >
+                            <ChatIcon />
+                        </Feature>
+                    </FeaturesRow>
+                </Hero>
+
+                <DetailedFeatures features={detailedFeatures} />
+            </Layout>
+        </main>
+    );
+};
 
 export default ContributePage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,111 +4,70 @@ import {
     CodeIcon,
     CubeTransparentIcon,
 } from "@heroicons/react/outline";
+import {
+    ArrowsExpandIconThin,
+    BookOpenIconThin,
+} from "../components/icons/HeroiconsThinOutlineIcons";
 import Layout from "../components/Layout";
 import NovelChan from "../components/NovelChan";
 import Hero from "../components/Hero";
 import Feature from "../components/Feature";
 import FeaturesRow from "../components/FeaturesRow";
+import DetailedFeatures from "../components/DetailedFeatures";
 
-const IndexPage = () => (
-    <main className="bg-gray-100 min-h-screen">
-        <title>NovelRT</title>
-        <Layout>
-            <Hero
-                image={<NovelChan />}
-                title="Meet NovelRT."
-                description="NovelRT is a low-ceremony multi-platform open source game engine with a strong focus on 2D Game Development and first-class support for Novel Game Development."
-            >
-                <FeaturesRow>
-                    <Feature
-                        featureName="Cross-Platform"
-                        featureDescription="We make an active effort to bring NovelRT to as many platforms as we can, so you can focus on building your games. Right now we support Windows and Linux, with Web, MacOS, iOS and Android being actively developed!"
-                    >
-                        <DesktopComputerIcon />
-                    </Feature>
-                    <Feature
-                        featureName="Use what you love"
-                        featureDescription="C#, Ink, Lua, MoonScript or C++? The core of NovelRT is written in C++ to give you the performance you need, but we provide in-depth bindings for powerful ecosystems like .NET. You can't find you favorite language? Just contribute language bindings yourself!"
-                    >
-                        <CodeIcon />
-                    </Feature>
-                    <Feature
-                        featureName="Built for modularity"
-                        featureDescription="NovelRT features a well-crafted multi-threaded Entity Component System and promotes Data Oriented Design so that your codebase scales with your project. Batched rendering and high code reusability are a core focus for us."
-                    >
-                        <CubeTransparentIcon />
-                    </Feature>
-                </FeaturesRow>
-            </Hero>
-            <div
-                id="secondary-branding"
-                className="flex flex-col gap-16 xl:gap-20 items-center py-28 md:px-12 lg:px-36 xl:px-46 2xl:px-64 mx-24 md:mx-36 xl:mx-64 text-gray-800"
-            >
-                <div className="grid grid-cols-1 xl:grid-cols-2 gap-1 place-items-center">
-                    <div className="xl:mx-12  justify-self-center">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 xl:py-6 xl:px-6 w-16 h-16 xl:w-36 xl:h-36 shadow">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={1.25}
-                                    d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
-                                />
-                            </svg>
-                        </div>
-                    </div>
-                    <div>
-                        <h3 className="mt-2 xl:mt-0 font-semibold xl:font-normal text-center xl:text-left text-3xl xl:text-4xl">
-                            2D games made seamless.
-                        </h3>
-                        <p className="mt-2 text-center xl:text-left">
-                            NovelRT is built from the ground up to create 2D
-                            games. The tooling is designed to support any kind
-                            of 2D game you can imagine! From puzzlers,
-                            metroidvanias, platformers, RTS games or even just
-                            your average Tetris clone!
-                        </p>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 xl:grid-cols-2 gap-1 place-items-center">
-                    <div className="xl:mx-12 justify-self-center xl:col-start-2 xl:col-end-3 xl:row-start-1">
-                        <div className="bg-gradient-to-br text-gray-100 from-green-400 via-blue-500 to-pink-600 rounded-full py-3 px-3 xl:py-6 xl:px-6 w-16 h-16 xl:w-36 xl:h-36 shadow">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={1.25}
-                                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-                                />
-                            </svg>
-                        </div>
-                    </div>
-                    <div className="xl:col-start-1 xl:col-end-2  xl:row-start-1">
-                        <h3 className="mt-2 xl:mt-0 font-semibold xl:font-normal text-center xl:text-left text-3xl xl:text-4xl">
-                            Write narratives. Not boilerplate.
-                        </h3>
-                        <p className="mt-2 text-center xl:text-left">
-                            Tired of writing tools and burning out before you
-                            can actually write your visual novel script? Do you
-                            want a tool that has it integrated as part of the
-                            platform? NovelRT is built to support your inner
-                            playwright from the get go!
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    </main>
-);
+const IndexPage = () => {
+    const detailedFeatures = [
+        {
+            id: "seamless-2d",
+            icon: <ArrowsExpandIconThin />,
+            title: "2D games made seamless.",
+            content:
+                "NovelRT is built from the ground up to create 2D games. The tooling is designed to support any kind of 2D game you can imagine! From puzzlers, metroidvanias, platformers, RTS games or even just your average Tetris clone!",
+        },
+        {
+            id: "no-boilerplate",
+            icon: <BookOpenIconThin />,
+            title: "Write narratives. Not boilerplate.",
+            content:
+                "Tired of writing tools and burning out before you can actually write your visual novel script? Do you want a tool that has it integrated as part of the platform? NovelRT is built to support your inner playwright from the get go!",
+        },
+    ];
+
+    return (
+        <main className="bg-gray-100 min-h-screen">
+            <title>NovelRT</title>
+            <Layout>
+                <Hero
+                    image={<NovelChan />}
+                    title="Meet NovelRT."
+                    description="NovelRT is a low-ceremony multi-platform open source game engine with a strong focus on 2D Game Development and first-class support for Novel Game Development."
+                >
+                    <FeaturesRow>
+                        <Feature
+                            featureName="Cross-Platform"
+                            featureDescription="We make an active effort to bring NovelRT to as many platforms as we can, so you can focus on building your games. Right now we support Windows and Linux, with Web, MacOS, iOS and Android being actively developed!"
+                        >
+                            <DesktopComputerIcon />
+                        </Feature>
+                        <Feature
+                            featureName="Use what you love"
+                            featureDescription="C#, Ink, Lua, MoonScript or C++? The core of NovelRT is written in C++ to give you the performance you need, but we provide in-depth bindings for powerful ecosystems like .NET. You can't find you favorite language? Just contribute language bindings yourself!"
+                        >
+                            <CodeIcon />
+                        </Feature>
+                        <Feature
+                            featureName="Built for modularity"
+                            featureDescription="NovelRT features a well-crafted multi-threaded Entity Component System and promotes Data Oriented Design so that your codebase scales with your project. Batched rendering and high code reusability are a core focus for us."
+                        >
+                            <CubeTransparentIcon />
+                        </Feature>
+                    </FeaturesRow>
+                </Hero>
+
+                <DetailedFeatures features={detailedFeatures} />
+            </Layout>
+        </main>
+    );
+};
 
 export default IndexPage;
